08629759210d3c8c0fd9e1a10eaca7c648839da8
shadrul <shadrul100@gmail.com>
2021-12-31 20:40:02 +0530

    modified and tested

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'PromptInput',         # How you named your package folder (MyLib)
   packages = ['PromptInput'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A python library build over argument parser to prompt user for input.',   # Give a short description about your library
   author = 'Shadrul',                   # Type in your name
   author_email = 'shadrul100@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/shadrul/PromptInput',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/shadrul/PromptInput/archive/refs/tags/v-0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/shadrul/PromptInput/archive/refs/tags/version-0.3.tar.gz',    # I explain this later on
   keywords = ['Argument parser', 'Prompt Input'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           # 'validators',
